c3247f7bf5d1382ace9c39be22a4e65167802ff2
Joey <joeysun95@gmail.com>
2023-08-14 22:50:05 +0200

    v1

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
+
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+session = requests.Session()
 
+url = 'https://novelusb.com/novel-book/release-that-witch-novel/chapter-1'
+response = session.get(url, headers=headers)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    article_div = soup.find('div', id='chr-content')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(article_div)
+
+    # with open('articles.txt', 'w') as file:
+    #     for article in articles:
+    #         title = article.text.strip()
+    #         file.write(title + '\n')
+    #
+    # print(f"{len(articles)} articles scraped and saved to 'articles.txt'.")
+else:
+    print("Failed to fetch the webpage.")
+    print(response)
+
+# def scrape_articles(url):
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         soup = BeautifulSoup(response.text, 'html.parser')
+#         articles = soup.find_all('h2', class_='article-title')
+#
+#         with open('articles.txt', 'w') as file:
+#             for article in articles:
+#                 title = article.text.strip()
+#                 file.write(title + '\n')
+#
+#         print(f"{len(articles)} articles scraped and saved to 'articles.txt'.")
+#     else:
+#         print("Failed to fetch the webpage.")
+#
+#
+# if __name__ == "__main__":
+#     target_url = "https://novelusb.com/novel-book/release-that-witch-novel/chapter-1"  # Replace with the actual URL
+#     scrape_articles(target_url)
